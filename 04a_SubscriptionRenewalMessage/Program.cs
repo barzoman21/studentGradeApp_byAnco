2ee5c4e2a44d8d23b2805b81e9c7cb8afd141d15
Esat Comlekci <esatcomlekci@gmail.com>
2026-01-14 22:20:38 +0300

    Code save forgotten :p

--- 04a_SubscriptionRenewalMessage/Program.cs
+++ 04a_SubscriptionRenewalMessage/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿Random random = new Random();
+int daysLeft = random.Next(12);
+int discountPercentage = 0;
+
+Console.WriteLine(daysLeft);
+
+
+if (daysLeft >=6 && daysLeft <=10)
+{
+    Console.WriteLine("Your subscription will expire soon. Wanna renew?");
+}
+
+else if (daysLeft >=2 && daysLeft<=5)
+    {
+        discountPercentage = 10;
+        Console.WriteLine($"Your subscription expires in {daysLeft} days.");
+        Console.WriteLine($"Renew now and save {discountPercentage}!");
+    }
+
+else if (daysLeft ==1)
+{
+    discountPercentage = 20;
+    Console.WriteLine($"Your subscription expires within a day!");
+    Console.WriteLine($"Renew now and save {discountPercentage}!");
+}
+
+else if (daysLeft == 0)
+{
+    Console.WriteLine("Your subscription has ended. Visit our Website to start a new subscription");
+}
+
+else
+{
+    Console.WriteLine("Did nothin");
+}
